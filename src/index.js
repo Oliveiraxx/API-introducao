ae94552373e8b53a5e28ac00f4e97033f4a516a5
Oliveiraxx <132698379+Oliveiraxx@users.noreply.github.com>
2023-09-01 23:09:11 +0000

    Atualizando

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
+//index.js
 import dotenv from "dotenv";
 import express from "express";
 dotenv.config();
 import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
 
-const app = express();
-const port = 3000;
+const app = express(); // Instancia o Express
+const port = 3000; // Define a porta
 
+
+//index.js
 app.use(express.json());
 app.use(roteadorUsuario);
+app.use(roteadorLogin);
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
   res.json({
-    message: "API para CRUD usuario: link_gitgub",
+    nome: "any crislen", // Substitua pelo seu nome 
   });
 });
+
 
 app.listen(port, () => {
   // Um socket para "escutar" as requisições
